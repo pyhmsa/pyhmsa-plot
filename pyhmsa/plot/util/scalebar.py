b83e8892b86fa2a620742cfd7527a05a545c68bb
Philippe Pinard <philippe.pinard@gmail.com>
2015-11-10 21:20:13 +0100

    Ensure scale bar is on top of colorbar

--- pyhmsa/plot/util/scalebar.py
+++ pyhmsa/plot/util/scalebar.py
@@ -43,7 +43,7 @@ imp.reload(sys.modules['matplotlib'])
 
 class ScaleBar(Artist):
 
-    zorder = 5
+    zorder = 6
 
     _PREFERRED_VALUES = [1, 2, 5, 10, 15, 20, 25, 50, 75, 100, 125, 150, 200, 500, 750]
 
